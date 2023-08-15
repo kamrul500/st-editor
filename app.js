6d58f14d40bf331e9ad9ba12dd73eea9fc22ad6c
Md. Kamrul Hasan <kamrulhassan500@gmail.com>
2023-08-15 16:53:11 +0600

    gbdf

--- app.js
+++ app.js
@@ -19,16 +19,6 @@ function insertLink() {
 }
 
 
-function formatAsCode() {
-
-    const code = prompt('Enter code:');
-    if (code) {
-        const pre = document.createElement('pre');
-        pre.textContent = code;
-        document.getSelection().getRangeAt(0).insertNode(pre);
-    }
-}
-
 function clearEditor() {
     const editor = document.getElementById('editor');
     editor.innerHTML = '';
@@ -56,32 +46,39 @@ function toggleView() {
 
     htmlView = !htmlView;
 }
+
+
 $(document).ready(function () {
-    // Create the toolbar HTML
-    var toolbarHtml = `
-        <div id="toolbar">
-             <button onclick="execCommand('bold')"><b>B</b></button>
-        <button onclick="execCommand('italic')"><i>I</i></button>
-        <button onclick="execCommand('underline')"><u>U</u></button>
-        <button onclick="execCommand('insertUnorderedList')">&#x2022;</button>
-        <button onclick="execCommand('insertOrderedList')">&#35;</button>
-        <button onclick="execCommand('justifyLeft')"><div>&#x21e4;</div></button>
-        <button onclick="execCommand('justifyCenter')"><div>&#x21e5;</div></button>
-        <button onclick="execCommand('justifyRight')"><div>&#x21e2;</div></button>
-        <button onclick="execCommand('justifyFull')"><div>&#x21e1;</div></button>
+    var toolbarContent = `
+        
+            <div id="toolbar">
+             <button onclick="execCommand('bold')"><i class="fa fa-bold"></i></button>
+        <button onclick="execCommand('italic')"><i class="fa fa-italic"></i></button>
+        <button onclick="execCommand('underline')"><i class="fa fa-underline"></i></button>
+        <button onclick="execCommand('insertUnorderedList')"><i class="fa fa-list"></i></button>
+        <button onclick="execCommand('insertOrderedList')"><i class="fa fa-list-ol"></i></button>
+        <button onclick="execCommand('justifyLeft')"><div><i class="fa fa-align-left"></i></div></button>
+        <button onclick="execCommand('justifyCenter')"><div><i class="fa fa-align-center"></i></div></button>
+        <button onclick="execCommand('justifyRight')"><div><i class="fa fa-align-right"></i></div></button>
+        <button onclick="execCommand('justifyFull')"><div><i class="fa fa-align-justify"></i></div></button>
         <button onclick="applyHeading('h1')">H1</button>
         <button onclick="applyHeading('h2')">H2</button>
         <button onclick="applyHeading('h3')">H3</button>
         <button onclick="applyHeading('h4')">H4</button>
         <button onclick="applyHeading('h5')">H5</button>
         <button onclick="applyHeading('h6')">H6</button>
-        <button onclick="insertLink()">Link</button>
-        <button onclick="formatAsCode()">Code</button>
+        <button onclick="applyHeading('p')">p</button>
+        <button onclick="insertLink()"><i class="fa fa-link"></i></button> 
         <button onclick="clearEditor()">Clear</button>
-        <button onclick="toggleView()">Toggle HTML View</button>
+        <button onclick="toggleView()"><i class="fa fa-code"></i></button>
+       
+        </div>
+        <div id="editor-container">
+            <div id="editor" contenteditable="true"></div>
+            <div id="html-view" contenteditable="true"></div>
         </div>
     `;
 
-    // Find the target element by its ID and append the toolbar
-    $("#targetElement").html(toolbarHtml);
+    // Find the target element by its ID and insert the toolbar content
+    $("#st-editor").html(toolbarContent);
 });
